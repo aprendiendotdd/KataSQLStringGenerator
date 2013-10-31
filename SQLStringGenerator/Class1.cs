d6d8fee693e6fd8c90e69791ae0f71231cab60a0
joaqfer <joaquin.fdez.ruiz@gmail.com>
2013-10-31 13:28:14 +0100

    Test Delete_Tabla_Filtrando_Por_Tres_Campos Verde

--- SQLStringGenerator/Class1.cs
+++ SQLStringGenerator/Class1.cs
@@ -10,28 +10,36 @@ namespace SQLStringGenerator
   {
     private const string SELECT = "SELECT ";
     private const string FROM = " FROM ";
+    private const string DELETE = "DELETE";
+    private const string WHERE = " WHERE ";
+    private const string EQUAL = " = ";
+    private const string AND = " AND ";
 
     public string GetSentenceSelect(string table, string[] columns)
     {
       StringBuilder stringBuilder = new StringBuilder().Append(SELECT);
       for (int i = 0; i < columns.Length; i++)
       {
-        if (i == columns.Length - 1)
-          stringBuilder.Append(columns[i]);
-        else
-          stringBuilder.Append(columns[i] + ",");
+        stringBuilder.Append(columns[i]);
+        if (i != columns.Length - 1) stringBuilder.Append(",");
       }
       stringBuilder.Append(FROM + table);
       return stringBuilder.ToString();
     }
 
     public string GetSentenceDelete(string table) {
-      return "DELETE FROM " + table;
+      return DELETE + FROM + table;
     }
 
     public string GetSentenceDelete(string table, string[] columns, string[] values)
     {
-      return "DELETE FROM " + table + " WHERE " + columns[0] + " = " + values[0];
+      StringBuilder stringBuilder = new StringBuilder().Append(DELETE + FROM + table + WHERE);
+      for (int i = 0; i < columns.Length; i++)
+      {
+        stringBuilder.Append(columns[i] + EQUAL + values[i]);
+        if (i != columns.Length - 1) stringBuilder.Append(AND);
+      }
+      return stringBuilder.ToString();
     }
   }
 }
